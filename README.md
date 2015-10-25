089e7306a16efb36bcebf2eb0d764cea102abb7b
Christiaan Hees <mail@chees.info>
2015-10-25 19:56:39 +0100

    Use OAuth2 to get a logged in user

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-To get the dependencies once:
+Download gcloud:
 
+    https://cloud.google.com/sdk/
+
+Install the Python components:
+
+    gcloud components install app-engine-python
+
+Install virtualenv (not sure if this virtualenv stuff is actually needed but last time I completely broke my Python setup, so better be safe than sorry):
+
+    sudo easy_install pip
+    sudo pip install virtualenv
+
+Create an environment:
+
+    cd ..
+    virtualenv env
+
+Activate the environment (do this everytime you start working on the project):
+
+    source env/bin/activate
+
+Install dependencies:
+
+    cd straightshot-server
     pip install -t lib -r requirements.txt
 
+Run:
 
-To run:
+    dev_appserver.py .
 
-    gcloud preview app run app.yaml
+Deploy:
 
-
-To deploy:
-
-    gcloud preview app deploy app.yaml
+    #gcloud preview app deploy app.yaml
 
 
 To get local image resizing working I had to do:
--- main.py
+++ main.py
@@ -4,12 +4,43 @@ import cloudstorage as gcs
 import webapp2
 import jinja2
 import uuid
+import httplib2
 
+from apiclient import discovery
+from oauth2client import appengine
+from oauth2client import client
+from google.appengine.api import memcache
 from google.appengine.api import app_identity
 from google.appengine.ext import ndb
 from google.appengine.api import images
+#from google.appengine.api import users
 
-class MainPage(webapp2.RequestHandler):
+
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
+
+MISSING_CLIENT_SECRETS_MESSAGE = """
+<h1>Warning: Please configure OAuth 2.0</h1>
+<p>
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+</p>
+<p>
+<code>%s</code>.
+</p>
+<p>with information found on the <a
+href="https://code.google.com/apis/console">APIs Console</a>.
+</p>
+""" % CLIENT_SECRETS
+
+
+service = discovery.build("plus", "v1", http=httplib2.Http(memcache))
+decorator = appengine.oauth2decorator_from_clientsecrets(
+    CLIENT_SECRETS,
+    scope='https://www.googleapis.com/auth/plus.me',
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+
+class MainHandler(webapp2.RequestHandler):
     def get(self):
         files = File.query().order(-File.createdDate).fetch(10)
 
@@ -22,14 +53,14 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render({'files': files}))
 
 
-class Images(webapp2.RequestHandler):
+class ImagesHandler(webapp2.RequestHandler):
     def post(self):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.write('"ok"')
         logging.info('Uploaded file length: ' + str(len(self.request.body)))
 
-        id = str(uuid.uuid4())
+        id = uuid.uuid4().hex
         filename = '/' + app_identity.get_default_gcs_bucket_name()
         filename += '/' + id + '.png'
 
@@ -46,10 +77,38 @@ class Images(webapp2.RequestHandler):
         self.response.headers['Access-Control-Allow-Methods'] = 'POST'
 
 
+class TokenHandler(webapp2.RequestHandler):
+    @decorator.oauth_required
+    def get(self):
+        try:
+            http = decorator.http()
+            user = service.people().get(userId='me').execute(http=http)
+            logging.info('displayName: ' + user['displayName'])
+            self.response.write(str(user))
+        except client.AccessTokenRefreshError:
+            self.redirect('/')
+
+        #self.response.headers['Content-Type'] = 'application/json'
+
+        #gae_user = users.get_current_user()
+        #if gae_user:
+        #    user = User(id=gae_user.user_id(), token=uuid.uuid4().hex, email=gae_user.email())
+        #    user.put()
+        #    self.response.write(user.token)
+        #else:
+        #    return webapp2.redirect(users.create_login_url('/token'))
+
+
 class File(ndb.Model):
     createdDate = ndb.DateTimeProperty(auto_now_add=True, indexed=True)
-    userEmail = ndb.StringProperty(indexed=True)
-    userName = ndb.StringProperty(indexed=False)
+    #userEmail = ndb.StringProperty(indexed=True)
+    #userName = ndb.StringProperty(indexed=False)
+    # TODO point to a User instead of duplicating email and name fields?
+
+
+class User(ndb.Model):
+    token = ndb.StringProperty(indexed=True)
+    email = ndb.StringProperty(indexed=True)
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -59,6 +118,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/images', Images)
+    ('/', MainHandler),
+    ('/images', ImagesHandler),
+    ('/token', TokenHandler),
+    (decorator.callback_path, decorator.callback_handler())
 ], debug=True)
